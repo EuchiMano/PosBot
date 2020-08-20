940296d721f66da985f22242ef54caed4f8813e1
Marcelo Pereyra <marce.ps@outlook.es>
2020-08-20 03:00:19 -0400

    Last Version

--- PosgradoBot/Dialogs/RootDialog.cs
+++ PosgradoBot/Dialogs/RootDialog.cs
@@ -12,12 +12,13 @@ using PosgradoBot.Infrastructure.SendGridEmail;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace PosgradoBot.Dialogs
 {
-    public class RootDialog: ComponentDialog
+    public class RootDialog : ComponentDialog
     {
         private readonly ILuisService _luisService;
         private readonly IDataBaseService _databaseService;
@@ -36,13 +37,13 @@ namespace PosgradoBot.Dialogs
                 InitialProcess,
                 FinalProcess
             };
-            
+
             AddDialog(new PreinscriptionDialog(_databaseService, userState));
             AddDialog(new QualificationDialog(_databaseService));
             AddDialog(new PaysDialog(_databaseService));
             AddDialog(new AgentDialog(_databaseService, _sendGridEmailService));
             AddDialog(new CursesDialog(_databaseService));
-            
+
             AddDialog(new TextPrompt(nameof(TextPrompt)));
             AddDialog(new WaterfallDialog(nameof(WaterfallDialog), waterfallSteps));
             InitialDialogId = nameof(WaterfallDialog);
@@ -65,22 +66,11 @@ namespace PosgradoBot.Dialogs
                     case "Saludar":
                         await IntentSaludar(stepContext, luisResult, cancellationToken);
                         break;
-                    case "Agradecer":
+                    case "Despedirse":
                         await IntentAgradecer(stepContext, luisResult, cancellationToken);
-                        break;
-                    case "Despedir":
-                        await IntentDespedir(stepContext, luisResult, cancellationToken);
-                        break;
-                    case "VerOpciones":
-                        await IntentVerOpciones(stepContext, luisResult, cancellationToken);
-                        break;
-                    case "VerCentroContacto":
-                        await IntentVerCentroContacto(stepContext, luisResult, cancellationToken);
                         break;
                     case "Calificar":
                         return await IntentCalificar(stepContext, luisResult, cancellationToken);
-                    case "PreInscripcion":
-                        return await IntentPreInscripcion(stepContext, luisResult, cancellationToken);
                     case "VerCursos":
                         return await IntentVerCursos(stepContext, luisResult, cancellationToken);
                     case "AtencionPersonal":
@@ -94,7 +84,8 @@ namespace PosgradoBot.Dialogs
                         break;
                 }
             }
-            else {
+            else
+            {
                 await IntentNone(stepContext, luisResult, cancellationToken);
             }
             return await stepContext.NextAsync(cancellationToken: cancellationToken);
@@ -115,10 +106,10 @@ namespace PosgradoBot.Dialogs
             return await stepContext.BeginDialogAsync(nameof(CursesDialog), cancellationToken: cancellationToken);
         }
 
-        private async Task<DialogTurnResult> IntentPreInscripcion(WaterfallStepContext stepContext, RecognizerResult luisResult, CancellationToken cancellationToken)
-        {
-            return await stepContext.BeginDialogAsync(nameof(CreateAppointmentDialog), cancellationToken: cancellationToken);
-        }
+        //private async Task<DialogTurnResult> IntentPreInscripcion(WaterfallStepContext stepContext, RecognizerResult luisResult, CancellationToken cancellationToken)
+        //{
+        //    return await stepContext.BeginDialogAsync(nameof(CreateAppointmentDialog), cancellationToken: cancellationToken);
+        //}
 
         #region IntentLuis
         private async Task<DialogTurnResult> IntentCalificar(WaterfallStepContext stepContext, RecognizerResult luisResult, CancellationToken cancellationToken)
@@ -128,53 +119,47 @@ namespace PosgradoBot.Dialogs
 
         private async Task IntentSaludar(WaterfallStepContext stepContext, RecognizerResult luisResult, CancellationToken cancellationToken)
         {
-            await stepContext.Context.SendActivityAsync($"Hola que gusto verte", cancellationToken: cancellationToken);
+            await stepContext.Context.SendActivityAsync($"Hola que gusto verte, ¿te puedo ayudar en algo?", cancellationToken: cancellationToken);
         }
 
         private async Task IntentAgradecer(WaterfallStepContext stepContext, RecognizerResult luisResult, CancellationToken cancellationToken)
         {
-            await stepContext.Context.SendActivityAsync("No te preocupes, me gusta ayudar.", cancellationToken: cancellationToken);
+            await stepContext.Context.SendActivityAsync("Hasta luego, espero haber sido de ayuda.", cancellationToken: cancellationToken);
         }
 
-        private async Task IntentDespedir(WaterfallStepContext stepContext, RecognizerResult luisResult, CancellationToken cancellationToken)
-        {
-            await stepContext.Context.SendActivityAsync("Espero verte pronto", cancellationToken: cancellationToken);
-        }
+        //private async Task IntentDespedir(WaterfallStepContext stepContext, RecognizerResult luisResult, CancellationToken cancellationToken)
+        //{
+        //    await stepContext.Context.SendActivityAsync("Espero verte pronto", cancellationToken: cancellationToken);
+        //}
 
-        private async Task IntentVerOpciones(WaterfallStepContext stepContext, RecognizerResult luisResult, CancellationToken cancellationToken)
-        {
-            await stepContext.Context.SendActivityAsync("Aqui tengo mis opciones", cancellationToken: cancellationToken);
-            await MainOptionsCard.ToShow(stepContext, cancellationToken);
-        }
-
-        private async Task IntentVerCentroContacto(WaterfallStepContext stepContext, RecognizerResult luisResult, CancellationToken cancellationToken)
-        {
-            string phoneDetail = $"Nuestros numero de atencion son los siguientes:{Environment.NewLine}" +
-                $"📞 +591 3 3579545";
-
-            string addressDetail = $"🏢 Estamos ubicados en {Environment.NewLine} Radial 13, Tercer Anillo, Barrio Aeronautico";
-
-            await stepContext.Context.SendActivityAsync(phoneDetail, cancellationToken: cancellationToken);
-            await Task.Delay(1000);
-            await stepContext.Context.SendActivityAsync(addressDetail, cancellationToken: cancellationToken);
-            await Task.Delay(1000);
-            await stepContext.Context.SendActivityAsync("¿En qué más te puedo seguir ayudando?", cancellationToken: cancellationToken);
-        }
+        //private async Task IntentVerOpciones(WaterfallStepContext stepContext, RecognizerResult luisResult, CancellationToken cancellationToken)
+        //{
+        //    await stepContext.Context.SendActivityAsync("Aqui tengo mis opciones", cancellationToken: cancellationToken);
+        //    await MainOptionsCard.ToShow(stepContext, cancellationToken);
+        //}
 
         private async Task IntentNone(WaterfallStepContext stepContext, RecognizerResult luisResult, CancellationToken cancellationToken)
         {
             var resultQnA = await _qnaMakerAIService._qnaMakerResult.GetAnswersAsync(stepContext.Context);
-            var score = resultQnA.FirstOrDefault().Score;
+            var score = resultQnA.FirstOrDefault()?.Score;
+
             string response = resultQnA.FirstOrDefault()?.Answer;
 
             if (score >= 0.5)
             {
                 await stepContext.Context.SendActivityAsync(response, cancellationToken: cancellationToken);
             }
-            else {
+            else
+            {
                 await stepContext.Context.SendActivityAsync("No entiendo lo que me dices.", cancellationToken: cancellationToken);
                 //await Task.Delay(1000);
                 //await IntentVerOpciones(stepContext, luisResult, cancellationToken);
+                string summary2 = $"Puedo ayudarte con las siguiente opciones:" +
+                    $"{Environment.NewLine} *Consulta de cursos" +
+                    $"{Environment.NewLine} *Consulta de pagos" +
+                    $"{Environment.NewLine} *Atencion Personalizada" +
+                    $"{Environment.NewLine} *Preguntas Frecuentes sobre la EMI Posgrado";
+                await stepContext.Context.SendActivityAsync(summary2, cancellationToken: cancellationToken);
             }
             //await stepContext.ContinueDialogAsync();
         }
